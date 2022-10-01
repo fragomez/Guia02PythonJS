0be4d09a9a14af84d821a36a549f27ccc8cfa591
fragoba <franklin.gomez@o365.unab.edu.co>
2022-09-30 22:48:13 -0500

    Ejercicio03

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 function calc(numero1,numero2,numero3){
-    if (numero1 > numero2 && numero1 > numero3){
-        let resultado ="El número mayor es " + numero1;
-        return resultado;
-    } else if (numero2 > numero1 && numero2 > numero3){
-        let resultado = "El número mayor es " + numero2;
-        return resultado;
-    } else if (numero3 > numero1 && numero3 > numero2){
-        let resultado = "El número mayor es " + numero3;
-        return resultado;
+    document.querySelector("#mayor").innerHTML="";
+    document.querySelector("#menor").innerHTML="";
+    document.querySelector("#iguales").innerHTML="";
+    if (numero1 > numero2 && numero1 >= numero3){
+        document.querySelector("#mayor").innerHTML = "El número mayor es " + numero1;
+        if(numero2 > numero3){
+            document.querySelector("#menor").innerHTML = "El número menor es " + numero3;
+        } else {
+            document.querySelector("#menor").innerHTML = "El número menor es " + numero2;
+        }
+    } else if (numero2 > numero1 && numero2 >= numero3){
+        document.querySelector("#mayor").innerHTML = "El número mayor es " + numero2;
+        if(numero1 > numero3){
+            document.querySelector("#menor").innerHTML = "El número menor es " + numero1;
+        } else{
+            document.querySelector("#menor").innerHTML = "El número menor es " + numero3;
+        }
+    } else if (numero3 > numero1 && numero3 >= numero2){
+        document.querySelector("#mayor").innerHTML = "El número mayor es " + numero3;
+        if(numero1 > numero2){
+            document.querySelector("#menor").innerHTML = "El número menor es " + numero2;
+        } else {
+            document.querySelector("#menor").innerHTML = "El número menor es " + numero1;
+        }
     } else {
-        let resultado = "Los números ingresados son iguales";
-        return resultado;
+        document.querySelector("#iguales").innerHTML = "Los números ingresados son iguales";
     }
     
 } 
@@ -21,15 +35,16 @@ addEventListener("DOMContentLoaded", (e)=>{
         let numero1 = document.querySelector("#numero1").value;
         let numero2 = document.querySelector("#numero2").value;
         let numero3 = document.querySelector("#numero3").value;
-        let resultado = calc(numero1,numero2,numero3);
-        document.querySelector("#resultado").innerHTML = resultado;
+        calc(numero1,numero2,numero3);
     })
 })
 
 
 function limpiar(){
     document.querySelector("#guia2").reset();
-    document.querySelector("#resultado").innerHTML="";
+    document.querySelector("#mayor").innerHTML="";
+    document.querySelector("#menor").innerHTML="";
+    document.querySelector("#iguales").innerHTML="";
 }
 
 addEventListener('reset', limpiar);
