7f2cad9bb304c32596bcc6b86eb1519fb5a467d7
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-01 21:33:30 -0500

    Ejercicio08

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-function calc(factura){
-    const desc15 = 0.15;
-    let subtotal = factura * desc15;
-    let total = factura - subtotal;
-    return total;
-} 
+function calc(seleccionado){
+    let horas = document.querySelector("#horas").value;
+    if (seleccionado == "Planta"){
+        const sueldoBase = 20000;
+        let sueldo = sueldoBase * horas;
+        return sueldo;
+    } else if (seleccionado == "Administrativo"){
+        const sueldoBase = 10000;
+        let sueldo = sueldoBase * horas;
+        return sueldo;
+    } else {
+        document.querySelector("#resultado").innerHTML = "Debe seleccionar un tipo de empleado";
+    }
+}
 
 addEventListener("DOMContentLoaded", (e) => {
-    let calcular = document.querySelector("#guia2");
-    calcular.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let factura = document.querySelector("#factura").value;
-        if(factura > 130000){
-            let total = factura - (factura * 0.15);
-            document.querySelector("#resultado").innerHTML = "TOTAL A PAGAR: $" + total + " pesos (descuento del 15% aplicado)";
-        } else {
-            document.querySelector("#resultado").innerHTML = "TOTAL A PAGAR: $" + factura + " pesos";
-        }
+    let opcion = document.querySelector("#opcion");
+    opcion.addEventListener("change", (e) => {
+        let seleccionado = opcion.value;
+        let calcular = document.querySelector("#guia2");
+        calcular.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let sueldo = calc(seleccionado);
+            document.querySelector("#resultado").innerHTML = "El sueldo es de <strong>$" + sueldo + " pesos</strong>";
+        })
     })
 })
 
