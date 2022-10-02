48ae81358a25bae280cb3df0e4b0ff7b0a7f63ed
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-01 22:49:10 -0500

    Ejercicio10

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let pares = 0, impares = 0;
+/*let pares = 0, impares = 0;
 function calc(numero1,numero2,numero3,numero4){
     if(numero1 % 2 !=0){
         impares = parseInt(numero1) - impares;
@@ -36,10 +36,31 @@ addEventListener("DOMContentLoaded", (e) => {
     })
 })
 
+*/
+
+addEventListener("DOMContentLoaded", (e) => {
+    let calcular = document.querySelector("#guia2");
+    calcular.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let producto1 = document.querySelector("#producto1").value;
+        let producto2 = document.querySelector("#producto2").value;
+        let producto3 = document.querySelector("#producto3").value;
+        let producto4 = document.querySelector("#producto4").value;
+        let producto5 = document.querySelector("#producto5").value;
+        let subTotal = parseInt(producto1) + parseInt(producto2) + parseInt(producto3) + parseInt(producto4) + parseInt(producto5);
+        let iva = subTotal * 0.10;
+        let total = subTotal + iva;
+        document.querySelector("#subtotal").innerHTML = `El subtotal es de $ ${subTotal} pesos`;
+        document.querySelector("#iva").innerHTML = `El IVA es de $ ${iva} pesos`;
+        document.querySelector("#resultado").innerHTML = `El total es de $ ${total} pesos`;
+    })
+})
+
 function limpiar(){
     document.querySelector("#guia2").reset();
     document.querySelector("#resultado").innerHTML="";
-    document.querySelector("#impares").innerHTML="";
+    document.querySelector("#subtotal").innerHTML="";
+    document.querySelector("#iva").innerHTML="";
 }
 
 addEventListener('reset', limpiar);
