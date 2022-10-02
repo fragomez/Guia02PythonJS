72081913a91810f665a53729b30e28ae7e0d3f05
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-01 20:47:34 -0500

    Ejercicio06

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-function calc(lado1,lado2){
-    let area = lado1 * lado2;
-    return area;
+function calc(temperatura,presion){
+    let alerta = temperatura * temperatura;
+    return alerta;
 } 
 
 addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia2");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let lado1 = document.querySelector("#lado1").value;
-        let lado2 = document.querySelector("#lado2").value;
-        if(lado1 < 0 || lado2 < 0){
-            alert("Los lados deben ser positivos");
-            
+        document.querySelector("#alarma").style.display = 'none';
+        document.querySelector("#resultado").innerHTML="";
+        document.querySelector("#alarma").innerHTML="";
+        let temperatura = document.querySelector("#temperatura").value;
+        let presion = document.querySelector("#presion").value;
+        if(temperatura > 100){
+            document.querySelector("#alarma").style.display = 'block';
+            document.querySelector("#alarma").innerHTML="ALARMA: TEMPERATURA MUY ALTA";
+        } else if(presion > 200){
+            document.querySelector("#alarma").style.display = 'block';
+            document.querySelector("#alarma").innerHTML="ALARMA: TEMPERATURA MUY ALTA";
         } else {
-            let resultado = calc(lado1,lado2)
-            document.querySelector("#resultado").innerHTML = resultado;
+            document.querySelector("#resultado").innerHTML = "NORMAL";
         }
     })
 })
 
 function limpiar(){
     document.querySelector("#guia2").reset();
-    document.querySelector("#mayor").innerHTML="";
-    document.querySelector("#menor").innerHTML="";
-    document.querySelector("#iguales").innerHTML="";
+    document.querySelector("#resultado").innerHTML="";
+    document.querySelector("#alarma").style.display = 'none';
 }
 
 addEventListener('reset', limpiar);
